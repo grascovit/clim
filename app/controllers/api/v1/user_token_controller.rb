acf1106107c85fe7b779dd454f28e24fa30a7bfb
Gabriel Rascovit <rascovit@gmail.com>
2018-01-23 14:32:25 -0200

    serialize user along with JWT token on login

--- app/controllers/api/v1/user_token_controller.rb
+++ app/controllers/api/v1/user_token_controller.rb
@@ -3,6 +3,9 @@
 module Api
   module V1
     class UserTokenController < Knock::AuthTokenController
+      def create
+        render json: { user: entity, token: auth_token.token }, status: :created
+      end
     end
   end
 end
---
+++ spec/requests/api/v1/user_token_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+module Api
+  module V1
+    RSpec.describe 'UserTokenController', type: :request do
+      let(:user) { create(:user) }
+
+      let(:valid_params) do
+        {
+          email: user.email,
+          password: user.password
+        }
+      end
+
+      let(:invalid_params) do
+        {
+          email: 'wrong',
+          password: 'wrong'
+        }
+      end
+
+      describe 'POST #create' do
+        context 'with valid params' do
+          it 'authenticates the user' do
+            expect do
+              post api_v1_user_token_path, params: { auth: valid_params }
+            end.to change(User, :count).by(1)
+          end
+
+          it 'returns 201 http status' do
+            post api_v1_user_token_path, params: { auth: valid_params }
+
+            expect(response).to have_http_status(:created)
+          end
+
+          it 'returns a json with user and token' do
+            post api_v1_user_token_path, params: { auth: valid_params }
+
+            expect(response).to match_response_schema('v1/user_token')
+          end
+        end
+
+        context 'with invalid params' do
+          it 'returns 404 http status' do
+            post api_v1_user_token_path, params: { auth: invalid_params }
+
+            expect(response).to have_http_status(:not_found)
+          end
+        end
+      end
+    end
+  end
+end
