a1de421b67308665bf8802a853a87c17070e3b1b
Gabriel Rascovit <rascovit@gmail.com>
2018-01-27 13:54:09 -0200

    finish client request specs

--- spec/requests/api/v1/users/clients_spec.rb
+++ spec/requests/api/v1/users/clients_spec.rb
@@ -131,10 +131,112 @@ module Api
           end
         end
 
-        skip 'PUT/PATCH #update' do
+        describe 'PUT/PATCH #update' do
+          let(:client) { create(:client, user: user) }
+
+          context 'with valid params' do
+            let(:new_params) do
+              {
+                name: 'Client name',
+                phone: '+5562909203912'
+              }
+            end
+
+            before do
+              put api_v1_user_client_path(user, client),
+                  headers: authenticated_header(user),
+                  params: { client: new_params }
+            end
+
+            it 'updates client data' do
+              client.reload
+
+              expect(client.name).to eq(new_params[:name])
+            end
+
+            it 'returns 200 http status' do
+              expect(response).to have_http_status(:ok)
+            end
+
+            it 'returns the updated user' do
+              expect(response).to match_response_schema('v1/client')
+            end
+          end
+
+          context 'with invalid params' do
+            it 'does not update client data' do
+              expect do
+                put api_v1_user_client_path(user, client),
+                    headers: authenticated_header(user),
+                    params: { client: invalid_params }
+                client.reload
+              end.not_to change(client, :name)
+            end
+
+            it 'returns 422 http status' do
+              put api_v1_user_client_path(user, client),
+                  headers: authenticated_header(user),
+                  params: { client: invalid_params }
+
+              expect(response).to have_http_status(:unprocessable_entity)
+            end
+
+            it 'returns the client errors' do
+              put api_v1_user_client_path(user, client),
+                  headers: authenticated_header(user),
+                  params: { client: invalid_params }
+
+              expect(JSON.parse(response.body)).to match_array(
+                [
+                  "Name can't be blank",
+                  "Phone can't be blank"
+                ]
+              )
+            end
+          end
+
+          context 'when client does not belong to current user' do
+            let(:another_client) { create(:client) }
+
+            it 'raises not found exception' do
+              expect do
+                put api_v1_user_client_path(user, another_client),
+                    headers: authenticated_header(user),
+                    params: { client: valid_params }
+              end.to raise_error(ActiveRecord::RecordNotFound)
+            end
+          end
         end
 
-        skip 'DELETE #destroy' do
+        describe 'DELETE #destroy' do
+          context 'when client belongs to current user' do
+            let!(:client) { create(:client, user: user) }
+
+            it 'deletes the client' do
+              expect do
+                delete api_v1_user_client_path(user, client),
+                       headers: authenticated_header(user)
+              end.to change(Client, :count).by(-1)
+            end
+
+            it 'returns 204 http status' do
+              delete api_v1_user_client_path(user, client),
+                     headers: authenticated_header(user)
+
+              expect(response).to have_http_status(:no_content)
+            end
+          end
+
+          context 'when client does not belong to current user' do
+            let!(:another_client) { create(:client) }
+
+            it 'raises not found exception' do
+              expect do
+                delete api_v1_user_client_path(user, another_client),
+                       headers: authenticated_header(user)
+              end.to raise_error(ActiveRecord::RecordNotFound)
+            end
+          end
         end
       end
     end
