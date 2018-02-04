37bd910877eb7bf1ffc9c1afce5792ce485ab541
Gabriel Rascovit <rascovit@gmail.com>
2018-02-03 23:14:11 -0200

    create tasks controller

---
+++ app/controllers/api/v1/clients/tasks_controller.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module Clients
+      class TasksController < ApplicationController
+        before_action :set_client
+        before_action :set_task, only: %i[show update destroy]
+
+        def index
+          render json: @client.tasks, status: :ok
+        end
+
+        def show
+          render json: @task, status: :ok
+        end
+
+        def create
+          @task = @client.tasks.new(task_params)
+
+          if @task.save
+            render json: @task, status: :created
+          else
+            render json: @task.errors.full_messages, status: :unprocessable_entity
+          end
+        end
+
+        def update
+          if @task.update(task_params)
+            render json: @task, status: :ok
+          else
+            render json: @task.errors.full_messages, status: :unprocessable_entity
+          end
+        end
+
+        def destroy
+          @task.destroy
+
+          head :no_content
+        end
+
+        private
+
+        def task_params
+          params.require(:task).permit(
+            :title,
+            :description,
+            :start_at,
+            :finish_at,
+            :service_fee
+          )
+        end
+
+        def set_client
+          @client = current_user.clients.find(params[:client_id])
+        end
+
+        def set_task
+          @task = @client.tasks.find(params[:id])
+        end
+      end
+    end
+  end
+end
