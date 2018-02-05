22e0518d17a5bb322c927663e4ae37c80c903453
Gabriel Rascovit <rascovit@gmail.com>
2018-02-05 20:50:53 -0200

    sort and paginate clients

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe Client, type: :model do
     it { is_expected.to validate_presence_of(:name) }
     it { is_expected.to validate_presence_of(:phone) }
   end
+
+  describe 'scopes' do
+    describe '.sorted_by_name' do
+      it 'returns the clients sorted by alphabetical order' do
+        client_a = create(:client, name: 'A')
+        client_b = create(:client, name: 'B')
+        client_c = create(:client, name: 'C')
+
+        expect(Client.sorted_by_name).to eq([client_a, client_b, client_c])
+      end
+    end
+  end
 end
